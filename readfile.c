4d47c02b1530d8ca17b6c0d239b4bb2c927db7f5
LKhoon <gnsdlrl@daum.net>
2020-08-26 22:27:59 +0900

    8/26.10pm

--- readfile.c
+++ readfile.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #define MAX_BUF 1024
-char g_filename[] = "test.txt";
+char	g_filename[] = "test.txt";
 
 int		**gridinit(int collen, int rowlen)
 {
@@ -20,15 +20,15 @@ int		**gridinit(int collen, int rowlen)
 	return (grid);
 }
 
-int readnext(int alreadyread)
+int lenfor_nextenter(int alreadyread)
 {
-	int 	fd;
-    int     count;
+    int		fd;
+	int     count;
     int     result;
     int     temp;
     char    buf[MAX_BUF];
-    result = 0;
 
+	result = 0;
 	fd = open(g_filename, O_RDONLY);
 	read(fd, buf, alreadyread);
     while ((temp = read(fd, buf, sizeof(buf))) > 0)
@@ -45,22 +45,64 @@ int readnext(int alreadyread)
     return (-1);
 }
 
-int rowlen(int	rowindex)
+int	howmany_charbefore(int	rowindex)
 {
-	int alreadyread;
-	int i;
+	int		i;
+	int		readstart;
 
 	i = 1;
-	alreadyread = 0;
+	readstart = 0;
 	while(i < rowindex)
 	{
-		alreadyread += readnext(alreadyread);
+		readstart += lenfor_nextenter(readstart);
+		if (lenfor_nextenter(readstart) == -1)
+			return (-1);
 		++i;
 	}
-	return (readnext(alreadyread) - 1);
+	return (readstart);
 }
+
+int rowlen(int	rowindex)
+{
+	int readstart;
+
+	readstart = howmany_charbefore(rowindex);
+	return (lenfor_nextenter(readstart) - 1);
+}
+
+
+char	*getcontents(int rowindex)
+{
+	char	buf[MAX_BUF];
+	char	*contents;
+	int		temp;
+	int		count;
+	int 	fd;
+
+	contents = (char*)malloc(sizeof(char) * rowlen(rowindex) + 1);
+	fd = open(g_filename, O_RDONLY);
+	read(fd, buf, howmany_charbefore(rowindex));
+	while ((temp = read(fd, buf, sizeof(buf))) > 0)
+	{
+		count = 0;
+		while (buf[count] != '\n' && count < temp)
+		{
+			*contents = buf[count++];
+			contents++;
+		}
+		if (buf[count] == '\n')
+		{
+			*contents = '\0';
+			return (contents - rowlen(rowindex));
+		}
+	}
+	return (0);
+}
+
 
 int main(void)
 {
-	printf("%d", rowlen(17));
+	printf("%d\n", rowlen(1));
+	printf("%d\n", howmany_charbefore(3));
+	printf("%s\n", getcontents(1));
 }
