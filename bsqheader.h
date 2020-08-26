899a75206971a4193cfd4df3b0cfa2b574858e82
minje70 <minje70@naver.com>
2020-08-26 22:57:33 +0900

    add header

---
+++ bsqheader.h
@@ -0,0 +1,15 @@
+#ifndef BSQHEADER
+# define BSQHEADER
+
+# define MAX_BUF 1024
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+char	g_filename[] = “test.txt”;
+
+int		rowlen(int	rowindex);
+
+#endif
